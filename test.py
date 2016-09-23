8277014034dffd745b6408db9fd018c7a1f8473f
Eeo Jun <141bytes@gmail.com>
2016-09-23 18:48:09 +0800

    Update!

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 from pprint import pprint
 from rqtree import Region
+from itertools import islice
+import random
 
 print('5x5:')
 r = Region.from_data([[0, 1, 2, 3, 4]]*5, target=0)
-b = [k for k in r.partition()]
-pprint(b)
-
-for region in b:
-    for k in region.partition():
-        print(k)
-        for g in k.partition():
-            if g != k:
-                print(' ', g)
-
 pprint(r.fill())
+
+
+def stream(a, b):
+    while True:
+        yield random.randint(a, b)
+
+
+for m in islice(stream(1, 10), 10):
+    for n in islice(stream(1, 10), 5):
+        buff = [[random.randint(1, 10) for _ in range(n)] for _ in range(m)]
+        r = Region.from_data(buff, target=0)
+        assert buff == r.fill()
